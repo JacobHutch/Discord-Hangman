3dfd665079b835af9d16ccedc788e7f113c0b185
Daniel Revis <drevis@gmu.edu>
2020-10-08 17:28:28 -0400

    Added word selection

--- HangStudent/HangStudent.cs
+++ HangStudent/HangStudent.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
@@ -8,10 +10,14 @@ namespace HangStudent
     class HangStudent
     {
         //================================== VARIABLES =================================================
-        // string word = "";
-        // List<char> guessed;
-        // bool gameStarted = false;
-        // int tries = 0;
+        private string word = "";
+        private List<char> guessed;
+        private bool gameStarted = false;
+        private int tries = 0;
+        private Random rand = new Random();
+        private static string tmpPath = Directory.GetCurrentDirectory();
+        private string wordlistPath = tmpPath.Substring(0, tmpPath.Length - 23) + "wordlist.txt";
+        //==============================================================================================
 
         static void Main(string[] args)
         => new HangStudent().MainAsync().GetAwaiter().GetResult();
@@ -34,10 +40,10 @@ namespace HangStudent
 
         private Task MessageHandler(SocketMessage msg)
         {
-            /*================================== BOT LOGIC HERE =================================================
+            /*====================================== PSEUDOCODE =================================================
              *
              * if msg == !start:
-             *      while word < 6, pick random string from wordlist and assign it to word
+             *      while word.length < 6, pick random string from wordlist and assign it to word
              *      gameStarted = true
              *      tries = 6
              *      guessed = new List<char>()
@@ -62,6 +68,41 @@ namespace HangStudent
              *
              *===================================================================================================
              */
+
+
+
+            //======================================= BOT LOGIC HERE ============================================
+
+            string userMsg = msg.Content;
+
+            if (userMsg.Equals("!start"))
+            {
+                msg.Channel.SendMessageAsync("**(This should be a game start message)**");
+
+                while (word.Length < 6)
+                {
+                    StreamReader file = new StreamReader(wordlistPath);
+
+                    int val = rand.Next() % 10000;
+                    for (int i = 1; i < val; i++)
+                    {
+                        file.ReadLine();
+                    }
+
+                    word = file.ReadLine();
+                    word.Substring(0, word.Length - 1);
+
+                    file.Close();
+                }
+
+                gameStarted = true;
+                tries = 6;
+                guessed = new List<char>();
+
+                //msg.Channel.SendMessageAsync($@"(DEBUG) Word chosen: {word}");
+            }
+
+            //===================================================================================================
 
             return Task.CompletedTask;
         }
