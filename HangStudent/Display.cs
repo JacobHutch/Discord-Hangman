9eb61e73a7de50d73762617cf48fa2b42550e65d
cjharrington <71154818+cjharrington@users.noreply.github.com>
2020-10-13 23:13:11 -0400

    added logic to display a game state

--- HangStudent/Display.cs
+++ HangStudent/Display.cs
@@ -8,40 +8,77 @@ namespace HangStudent {
 		private string IMAGES_DIRECTORY = Directory.GetCurrentDirectory()+"/img/";
 		private string[] phasePaths;
 
-		public Display() {
-			/* initialize images */
-			phasePaths = new string[10];
-			for (int i = 0; i < 10; ++i) {
-				phasePaths[i] = IMAGES_DIRECTORY+"phase"+(i+1)+".png";
-			}
-		}
-
-		public void GameStart(ISocketMessageChannel channel) {
-			SendMessage(channel, "Game start! You get 6 guesses.\n\\_ \\_ \\_", phasePaths[0]);
+		public void GameStart(GameInfo state) {
+			SendMessage(ref state, "Some text", true);
 		}
 
 		public void CorrectGuess(ISocketMessageChannel channel) {
-			SendMessage(channel, "'E' is correct! 6 guesses remain.\n\\_ E \\_", phasePaths[0]);
 		}
 
 		public void IncorrectGuess(ISocketMessageChannel channel) {
-			SendMessage(channel, "'A' is incorrect. 5 guesses remain.\n\\_ \\_ \\_", phasePaths[1]);
 		}
 
 		public void Win(ISocketMessageChannel channel) {
-			SendMessage(channel, "'E' is correct! The word is BET. Play again?");
 		}
 
 		public void Loss(ISocketMessageChannel channel) {
-			SendMessage(channel, "'A' is incorrect. The word was BET. Play again?");
 		}
 
-		private void SendMessage(ISocketMessageChannel channel, string text, string imageUri=null) {
-			if (imageUri != null) {
-				channel.SendFileAsync(imageUri, text);
+		private string SelectImage(int startTurns, int turns) {
+			if (startTurns <= 7) {
+				return phasePaths[7-turns];
+			}
+			// if 7 isn't enough, use the first 10 images instead
+			else if (startTurns <= 10) {
+				return phasePaths[10-turns];
+			}
+			// if there aren't enough images, repeat the first one until there are
+			else {
+				if (turns > 10)
+					return phasePaths[0];
+				else
+					return phasePaths[10-turns];
+			}
+		}
+
+		/* build the string of blanks and guessed letters */
+		private string BuildWordLine(string word, char[] correctLetters, char[] wrongLetters) {
+			string ret = "**";
+
+			/* blanks filled in with correct guesses */
+			foreach (char c in word) {
+				bool found = false;
+				foreach (char guess in correctLetters) {
+					if (guess == c) {
+						found = true;
+						break;
+					}
+				}
+				if (found) {
+					ret += c + " ";
+				}
+				else {
+					ret += "\\_ ";
+				}
+			}
+
+			/* list of incorrect guesses */
+			ret += "** (";
+			for (int i = 0; i < wrongLetters.Length - 1; ++i) {
+				ret += wrongLetters[i] + ", ";
+			}
+			ret += wrongLetters[wrongLetters.Length - 1] + ")";
+
+			return ret;
+		}
+
+		private void SendMessage(ref GameInfo state, string text, bool showGame=false) {
+			if (showGame) {
+				state.channel.SendFileAsync(SelectImage(state.startTurns, state.turns),
+						text+"\n"+BuildWordLine(state.word, state.correctLetters, state.wrongLetters));
 			}
 			else {
-				channel.SendMessageAsync(text);
+				state.channel.SendMessageAsync(text);
 			}
 		}
 
